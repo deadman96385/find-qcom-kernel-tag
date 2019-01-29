748dd9787f2e52c34184e79d204fe103cb78fe31
Sean Hoyt <seanhoyt963@gmail.com>
2019-01-28 22:04:55 -0600

    Don't use my local alias for git show

--- find_tags.sh
+++ find_tags.sh
@@ -7,7 +7,7 @@ REPO_DIR="android_kernel_lenovo_msm8909/"
 old_result=999999999
 
 for tag in `cat ${tags_file}`; do
-	tag_hash=`git -C ${REPO_DIR} sh -s --format=%H ${tag} | tail -n1`
+	tag_hash=`git -C ${REPO_DIR} show -s --format=%H ${tag} | tail -n1`
 	git -C ${REPO_DIR} fetch caf ${tag_hash} >> /dev/null 2>&1
 	git -c diff.renameLimit=9999 -C ${REPO_DIR} diff ${tag_hash} --shortstat >> ${out_file}
 	result=`echo egrep -o '[0-9][0-9]* files changed'|egrep -o '[0-9][0-9]*' report.txt| dd status=none bs=1 count=5`
