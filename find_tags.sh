7901dc23216baba436ed8185bab755c45072f23c
Stephen <shm@debian.shm>
2019-03-01 14:35:35 -0500

    BUG: Fix bad integer if number of changed files is less
    This change no longer relies on DD under the assumption that we count 5 to grab the value of changed files.
    If the number of changed files was a 4 digit value then it would suffice, but anything less or greater would incidently take the next output from the total number of insertions
    creating a multiline, inaccurate, answer.
    
    For example, if report.txt displayed:
    
    701 files changed, 300109 insertions(+), 56549 deletions(-)
    
    Then the following command from:
    
    echo egrep -o '[0-9][0-9]* files
    changed' | egrep -o '[0-9][0-9]*' report.txt | dd status=none bs=1 count=5
    
    Would return:
    
    701
    3
    
    Instead of properly returning 701.
    
    To resolve this bug, use the variable, result, as an array, and call on the
    first output from that array while ignoring the rest and getting the exact results we need regardless of the number of changed files found and eliminating the return of a bad integer.

--- find_tags.sh
+++ find_tags.sh
@@ -10,12 +10,12 @@ for tag in `cat ${tags_file}`; do
 	tag_hash=`git -C ${REPO_DIR} show -s --format=%H ${tag} | tail -n1`
 	git -C ${REPO_DIR} fetch caf ${tag_hash} >> /dev/null 2>&1
 	git -c diff.renameLimit=9999 -C ${REPO_DIR} diff ${tag_hash} --shortstat >> ${out_file}
-	result=`echo egrep -o '[0-9][0-9]* files changed'|egrep -o '[0-9][0-9]*' report.txt| dd status=none bs=1 count=5`
+	result=( $(echo egrep -o '[0-9][0-9]* files changed' | egrep -o '[0-9][0-9]*' report.txt) )
 	rm ${out_file}
-	if [ "$result" -le "$old_result" ]; then
-		old_result=$result
+	if [ "${result[0]}" -le "$old_result" ]; then
+		old_result=${result[0]}
 		best_tag=${tag}
-		echo -e "New best tag:     ${tag}, files changed: ${result}"
+		echo -e "New best tag:     ${tag}, files changed: ${result[0]}"
 	else
 		echo -e "Current best tag: ${best_tag}, files changed: ${old_result}"
 	fi
