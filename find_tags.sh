e270dadb470835d847ed00cad6b1bb7153f8c789
Stephen <shm@debian.shm>
2019-03-11 05:08:27 -0500

    Touch up codebase to conform more appropriately with BASH
    -- SC2013: To read lines rather than words, pipe/redirect to a 'while read' loop.
    -- SC2006: Use $(...) notation instead of legacy backticked `...`.
    -- SC2086: Double quote to prevent globbing and word splitting.
    -- SC2196: egrep is non-standard and deprecated. Use grep -E instead.
    -- SC2207: Prefer mapfile or read -a to split command output (or quote to avoid splitting).

--- find_tags.sh
+++ find_tags.sh
@@ -6,15 +6,15 @@ REPO_DIR="android_kernel_lenovo_msm8909/"
 
 old_result=999999999
 
-for tag in `cat ${tags_file}`; do
-	tag_hash=`git -C ${REPO_DIR} show -s --format=%H ${tag} | tail -n1`
-	git -C ${REPO_DIR} fetch caf ${tag_hash} >> /dev/null 2>&1
-	git -c diff.renameLimit=9999 -C ${REPO_DIR} diff ${tag_hash} --shortstat >> ${out_file}
-	result=( $(echo egrep -o '[0-9][0-9]* files changed' | egrep -o '[0-9][0-9]*' report.txt) )
-	rm ${out_file}
+grep -v '^ *#' < "${tags_file}" | while IFS= read -r tag; do
+	tag_hash=$(git -C "${REPO_DIR}" show -s --format=%H "${tag}" | tail -n1)
+	git -C "${REPO_DIR}" fetch caf "${tag_hash}" >> /dev/null 2>&1
+	git -c diff.renameLimit=9999 -C "${REPO_DIR}" diff "${tag_hash}" --shortstat >> "${out_file}"
+	mapfile -t result < <(echo grep -Eo '[0-9][0-9]* files changed' | grep -Eo '[0-9][0-9]*' report.txt)
+	rm "${out_file}"
 	if [ "${result[0]}" -le "$old_result" ]; then
-		old_result=${result[0]}
-		best_tag=${tag}
+		old_result="${result[0]}"
+		best_tag="${tag}"
 		echo -e "New best tag:     ${tag}, files changed: ${result[0]}"
 	else
 		echo -e "Current best tag: ${best_tag}, files changed: ${old_result}"
